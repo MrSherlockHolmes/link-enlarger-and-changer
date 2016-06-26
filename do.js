e31f5108ed8d8f848882ca05c16d4b4a63859ec6
Sabri <MrSherlockHolmes123@gmail.com>
2016-06-26 11:35:42 +0100

    Update do.js

--- do.js
+++ do.js
@@ -1,1 +1,32 @@
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
 
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+var websites = ["billionbirthday", "alightyear", "jackthefabricripper", "to", "uk"]
+checkCookie();
+function checkCookie() {
+    if (getCookie("siteNumber") === "" || getCookie("siteNumber") === "4") {
+      setCookie("siteNumber", "0", 365);
+    } else {
+      setCookie("siteNumber", getCookie("siteNumber") + 1, 365);
+    }
+    window.location.href = "http://" + websites[getCookie("siteNumber")];
+}
